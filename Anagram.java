6e12906fe585a91c53ec6b2311a36d8a6225b772
samuelelkaim <elkaim.samuel03@gmail.com>
2024-11-28 16:02:55 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,49 +1,58 @@
-/** Functions for checking if a given string is an anagram. */
 public class Anagram {
-	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+    // Preprocess the string: Remove non-alphabetic characters and convert to lowercase
+    public static String preProcess(String str) {
+        return str.replaceAll("[^a-zA-Z]", "").toLowerCase();
+    }
 
-	// Returns true if the two given strings are anagrams, false otherwise.
-	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
-	} 
-	   
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
-	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
-	}
+    // Check if two strings are anagrams
+    public static boolean isAnagram(String str1, String str2) {
+        str1 = preProcess(str1);
+        str2 = preProcess(str2);
+
+        if (str1.length() != str2.length()) return false;
+
+        for (char c : str1.toCharArray()) {
+            int index = str2.indexOf(c);
+            if (index == -1) {
+                return false; // Character not found
+            }
+            str2 = str2.substring(0, index) + str2.substring(index + 1); // Remove character from str2
+        }
+        return true;
+    }
+
+    // Generate a random anagram of a string
+    public static String randomAnagram(String str) {
+        str = preProcess(str);
+        char[] chars = str.toCharArray();
+        shuffleArray(chars); // Shuffle the array of characters
+        return new String(chars);
+    }
+
+    // Shuffle a character array (Fisher-Yates shuffle algorithm)
+    private static void shuffleArray(char[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = (int) (Math.random() * (i + 1)); // Random index from 0 to i
+            // Swap array[i] and array[j]
+            char temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test preProcess function
+        System.out.println("PreProcess: " + preProcess("Nag a Ram!")); // Expected: "nagaram"
+
+        // Test isAnagram function
+        System.out.println("Is Anagram (listen, silent): " + isAnagram("listen", "silent")); // Expected: true
+        System.out.println("Is Anagram (hello, world): " + isAnagram("hello", "world"));   // Expected: false
+        System.out.println("Is Anagram (Nag a Ram, anagram): " + isAnagram("Nag a Ram", "anagram")); // Expected: true
+
+        // Test randomAnagram function
+        System.out.println("Random Anagram (java): " + randomAnagram("java")); // Output: Random permutation of "java"
+        System.out.println("Random Anagram (hello): " + randomAnagram("hello")); // Output: Random permutation of "hello"
+    }
 }
+
