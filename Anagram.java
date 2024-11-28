9636373d9557520bf4d8601fa5010ab4d7c446b8
samuelelkaim <elkaim.samuel03@gmail.com>
2024-11-28 16:35:16 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,6 +1,8 @@
+import java.util.Random;
+
 public class Anagram {
 
-    // Preprocess the string
+    // Preprocess the string: Remove non-alphabetic characters and convert to lowercase
     public static String preProcess(String str) {
         return str.replaceAll("[^a-zA-Z]", "").toLowerCase();
     }
@@ -14,23 +16,25 @@ public class Anagram {
 
         for (char c : str1.toCharArray()) {
             int index = str2.indexOf(c);
-            if (index == -1) return false;
-            str2 = str2.substring(0, index) + str2.substring(index + 1);
+            if (index == -1) return false; // Character not found
+            str2 = str2.substring(0, index) + str2.substring(index + 1); // Remove character from str2
         }
         return true;
     }
 
     // Generate a random anagram
     public static String randomAnagram(String str) {
-        char[] chars = preProcess(str).toCharArray();
-        shuffleArray(chars);
+        str = preProcess(str); // Remove spaces and non-alphabetic characters
+        char[] chars = str.toCharArray();
+        shuffleArray(chars); // Shuffle the array of characters
         return new String(chars);
     }
 
     // Shuffle array using Fisher-Yates algorithm
     private static void shuffleArray(char[] array) {
+        Random random = new Random();
         for (int i = array.length - 1; i > 0; i--) {
-            int j = (int) (Math.random() * (i + 1));
+            int j = random.nextInt(i + 1);
             char temp = array[i];
             array[i] = array[j];
             array[j] = temp;
@@ -38,10 +42,22 @@ public class Anagram {
     }
 
     public static void main(String[] args) {
-        System.out.println("PreProcess: " + preProcess("Nag a Ram!")); // Expected: "nagaram"
+        // Test preProcess method
+        System.out.println("PreProcess (simple): " + preProcess("Nag a Ram!")); // Expected: "nagaram"
+        System.out.println("PreProcess (spaces): " + preProcess("Hello World!")); // Expected: "helloworld"
+        System.out.println("PreProcess (case): " + preProcess("ABCdef")); // Expected: "abcdef"
+        System.out.println("PreProcess (empty): " + preProcess("")); // Expected: ""
+
+        // Test isAnagram method
         System.out.println("Is Anagram (listen, silent): " + isAnagram("listen", "silent")); // Expected: true
         System.out.println("Is Anagram (hello, world): " + isAnagram("hello", "world"));   // Expected: false
         System.out.println("Is Anagram (Nag a Ram, anagram): " + isAnagram("Nag a Ram", "anagram")); // Expected: true
-        System.out.println("Random Anagram (java): " + randomAnagram("java")); // Random permutation
+
+        // Test randomAnagram method
+        System.out.println("Random Anagram (java): " + randomAnagram("java")); // Random permutation of "java"
+        System.out.println("Random Anagram (hello): " + randomAnagram("hello")); // Random permutation of "hello"
     }
 }
+
+
+ 
