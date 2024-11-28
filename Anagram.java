fe3fae554aadc0b6b2d53d95b2414cbd2e9bfa7c
samuelelkaim <elkaim.samuel03@gmail.com>
2024-11-28 16:20:08 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,6 +1,6 @@
 public class Anagram {
 
-    // Preprocess the string: Remove non-alphabetic characters and convert to lowercase
+    // Preprocess the string
     public static String preProcess(String str) {
         return str.replaceAll("[^a-zA-Z]", "").toLowerCase();
     }
@@ -14,27 +14,23 @@ public class Anagram {
 
         for (char c : str1.toCharArray()) {
             int index = str2.indexOf(c);
-            if (index == -1) {
-                return false; // Character not found
-            }
-            str2 = str2.substring(0, index) + str2.substring(index + 1); // Remove character from str2
+            if (index == -1) return false;
+            str2 = str2.substring(0, index) + str2.substring(index + 1);
         }
         return true;
     }
 
-    // Generate a random anagram of a string
+    // Generate a random anagram
     public static String randomAnagram(String str) {
-        str = preProcess(str);
-        char[] chars = str.toCharArray();
-        shuffleArray(chars); // Shuffle the array of characters
+        char[] chars = preProcess(str).toCharArray();
+        shuffleArray(chars);
         return new String(chars);
     }
 
-    // Shuffle a character array (Fisher-Yates shuffle algorithm)
+    // Shuffle array using Fisher-Yates algorithm
     private static void shuffleArray(char[] array) {
         for (int i = array.length - 1; i > 0; i--) {
-            int j = (int) (Math.random() * (i + 1)); // Random index from 0 to i
-            // Swap array[i] and array[j]
+            int j = (int) (Math.random() * (i + 1));
             char temp = array[i];
             array[i] = array[j];
             array[j] = temp;
@@ -42,17 +38,10 @@ public class Anagram {
     }
 
     public static void main(String[] args) {
-        // Test preProcess function
         System.out.println("PreProcess: " + preProcess("Nag a Ram!")); // Expected: "nagaram"
-
-        // Test isAnagram function
         System.out.println("Is Anagram (listen, silent): " + isAnagram("listen", "silent")); // Expected: true
         System.out.println("Is Anagram (hello, world): " + isAnagram("hello", "world"));   // Expected: false
         System.out.println("Is Anagram (Nag a Ram, anagram): " + isAnagram("Nag a Ram", "anagram")); // Expected: true
-
-        // Test randomAnagram function
-        System.out.println("Random Anagram (java): " + randomAnagram("java")); // Output: Random permutation of "java"
-        System.out.println("Random Anagram (hello): " + randomAnagram("hello")); // Output: Random permutation of "hello"
+        System.out.println("Random Anagram (java): " + randomAnagram("java")); // Random permutation
     }
 }
-
